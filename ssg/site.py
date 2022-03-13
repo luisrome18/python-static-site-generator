145ecad27029ce96378e65b7cb02701e3552c014
luisrome18 <53321335+luisrome18@users.noreply.github.com>
2022-03-13 19:38:37 -0400

    The Parser Class

--- ssg/site.py
+++ ssg/site.py
@@ -32,17 +32,17 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-            elif path.isfile:
+            elif path.isfile():
                 self.run_parser(path)
 
     def load_parser(self, extension):
-            for parser in self.parser:
-                if parser.valid_extension(extension):
-                    return parser
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
 
-    def run_parser(path):
+    def run_parser(self, path):
         parser = self.load_parser(path.suffix)
         if parser is not None:
-            parser.parse(path, source, dest)
+            parser.parse(path, source)
         else:
             print("Not Implemented")
