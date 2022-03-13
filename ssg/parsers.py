58fd2e98e89d1514d5fec85c314f8cf1f5e22abc
luisrome18 <53321335+luisrome18@users.noreply.github.com>
2022-03-13 11:47:47 -0400

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,5 +7,9 @@ class Parser:
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(self, path: Path, source: Path, dest: Path)
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
+
+    def read(self, path):
+        with open(path, "r") as file
+            return file.read()
